a784170b7eca8fc959ae6eb2f88a30fc259809b8
Ghostboa <ghostboa@gmail.com>
2014-12-11 22:09:36 -0500

    Added Basics

---
+++ Recursion_02.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+
+/*
+2.	A sequence is defined by the following recurrence relation:
+tn = 7t[n-1] - 4t[n-2] -9t[n-3]
+and the base case:
+t[1] = 0, t[2] = 5, t[3] = 3
+Write a function to determine t[n] given n.
+*/
+int calculate (int a, int b, int c, int n){
+
+}
+
+
+int main (){
+    int n = 0;
+
+}
