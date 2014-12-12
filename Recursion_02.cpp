ed1b9cb79f63484e031b29c5cbe68c78fcd8cbf9
Ghostboa <ghostboa@gmail.com>
2014-12-12 10:20:17 -0500

    Got work done today...sorta

--- Recursion_02.cpp
+++ Recursion_02.cpp
@@ -11,12 +11,13 @@ and the base case:
 t[1] = 0, t[2] = 5, t[3] = 3
 Write a function to determine t[n] given n.
 */
-int calculate (int a, int b, int c, int n){
-
+int calculate (int n){
+    if (n == 1)
+        return 0
 }
 
 
 int main (){
-    int n = 0;
-
+    int keyN = 0;
+    scanf ("%i", &keyN);
 }
---
+++ Recursive_02.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "time.h"
+#include "iostream"
+
+#define INITIALIZER 17
+/*
+2.	A sequence is defined by the following recurrence relation:
+and the base case:  . Write a function to determine   given  .
+*/
+
+int evaluate (){
+
+}
+
+int main (){
+
+
+}
