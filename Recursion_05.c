0364e870187cddb79bafad79738972a5795c0d38
Ghostboa <ghostboa@gmail.com>
2014-12-15 11:45:14 -0500

    Stuff

---
+++ Recursion_05.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*
+5.	Write a function to count the number of multiples of m between a and b in an array of integers A.
+Where b >=a, and a, b, and m are all whole numbers.
+NOTE that the multiples are between the whole numbers a and b … a and b are not indices in the array.
+*/
+
+int unamedFunction(){
+    if (str[i] == 0) return (0);
+    if (())
+
+}
+
+
+int main (){
+
+}
