b6a275a31ad5eea5093e9102b2871939229d86ed
Ghostboa <ghostboa@gmail.com>
2014-12-18 10:20:35 -0500

    End Thursday

---
+++ Recursion_06.c
@@ -0,0 +1,11 @@
+// 6.	An array contains positive integers but the last element is negative.
+// Write a function to compute the sum of the multiples of m in the array.
+
+/*
+This is a block comment.
+Use these.
+*/
+
+int main (){
+
+}
