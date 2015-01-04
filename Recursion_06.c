ef7634a35f18a5d17138b48a38c7710c8b3fd5f0
Ghostboa <ghostboa@gmail.com>
2015-01-04 11:48:06 -0500

    Complete: Recursion 6

--- Recursion_06.c
+++ Recursion_06.c
@@ -5,7 +5,18 @@
 This is a block comment.
 Use these.
 */
+int computeSum (int intArray [80], int m) {
+    if (intArray[0] < 0)
+        return 0;
+    if (intArray[0] % m == 0)
+        return (intArray[0] + computeSum (intArray + 1, m));
+    if (intArray[0] % m != 0)
+        return (computeSum (intArray +1, m));
+}
 
 int main (){
+    int intArray [80] = {0,1,2,3,4,5,6,7,8,9,-1};
 
+    int m = 2;
+    printf ("%i",computeSum(intArray,m));
 }
